b50a6673bbb2250936a432df406caea41e1c56d4
Wade Jones <wadedjones@gmail.com>
2024-08-16 18:21:22 -0700

    finished chapter 02, starting 03

---
+++ CH02/Homework/2.85.c
@@ -0,0 +1,10 @@
+/* Given a floating-point forma with a k-bit exponent and an n-bit fraction,
+ * write formulas for the exponent E, the significand M, the fraction f, and the
+ * value V for the quantities that follow. In addition, describe the bit
+ * representation.
+ *
+ * A. The number 7.0
+ * B. The largest odd integer that can be represented exactly
+ * C. The reciprocal of the smallest positive normalized value
+ *
+ */
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Starting Computer Systems: A Programmer's Perspective
 
+CH02
+- Very hard stuff. Like really hard stuff. But finished. Sorta.
+- Skipped a few of the homework problems. Woops.
+
 CSAPP
